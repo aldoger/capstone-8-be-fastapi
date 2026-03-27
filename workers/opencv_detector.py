bfd2c45765d72143e38e69345dc997ce7325897c
aldoger <nainggolanben12@gmail.com>
2026-03-27 11:28:45 +0700

    feat: load yolo model

--- workers/opencv_detector.py
+++ workers/opencv_detector.py
@@ -3,12 +3,15 @@ import time
 import uuid
 import requests
 from datetime import datetime
+from ultralytics import YOLO
 
 API_URL = "http://localhost:8000/detection"
 
+model = YOLO("yolo.pt")
 cap = cv2.VideoCapture(0)
 
 last_time = 0
+
 
 while True:
     ret, frame = cap.read()
@@ -16,28 +19,11 @@ while True:
     if not ret:
         break
 
-    now = time.time()
+    result = model(frame)
 
-    if now - last_time > 5:
-
-        payload = {
-            "detections": [
-                {
-                    "id": str(uuid.uuid4()),
-                    "object": "person",
-                    "timestamp": datetime.utcnow().isoformat()
-                }
-            ]
-        }
-
-        try:
-            requests.post(API_URL, json=payload)
-        except Exception as e:
-            print("Error:", e)
-
-        last_time = now
-
-    cv2.imshow("Webcam", frame)
+    annotated_frame = result[0].plot()
+        
+    cv2.imshow("YOLO", annotated_frame)
 
     if cv2.waitKey(1) == ord('q'):
         break
